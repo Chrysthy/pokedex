47c77efc12824298f3f6c19d2304fa01b453b5c1
Chrystine Martins <chrystine.martins@outlook.com>
2025-07-13 13:57:18 -0300

    add GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import { useState } from 'react'
 import './App.css'
+import { themes } from './styles/themes'
+import { ThemeProvider } from './contexts/ThemesContext'
+import { ThemeTogglerButton } from './components/ThemeTogglerButton/ThemeTogglerButton'
+import { createGlobalStyle } from 'styled-components'
 
 function App() {
-  
+
   return (
     <>
-      
+      <ThemeProvider>
+
+        <ThemeTogglerButton />
+
+      </ThemeProvider>
+
     </>
   )
 }
 
-export default App
+const GlobalStyle = createGlobalStyle`
+
+* {
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+`
+
+export default App;
