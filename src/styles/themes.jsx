fa9d45b76b4b0ed8d5d0efa0cdb3c3b06db1edf1
Chrystine Martins <chrystine.martins@outlook.com>
2025-07-13 12:04:04 -0300

    add themes

---
+++ src/styles/themes.jsx
@@ -0,0 +1,13 @@
+
+export const themes = {
+    light: {
+        background: "#ffffff",
+        color: "#000000",
+    },
+
+    dark: {
+        background: "#000000",
+        color: "#ffffff",
+    }
+}
+
