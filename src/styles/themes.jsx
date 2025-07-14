4f80cd188bac699a892d21561c533541bf871670
Chrystine Martins <chrystine.martins@outlook.com>
2025-07-14 19:58:05 -0300

    update code

--- src/styles/themes.jsx
+++ src/styles/themes.jsx
@@ -1,3 +1,6 @@
+import { createGlobalStyle } from "styled-components";
+import { useContext } from "react";
+import { ThemesContext } from "../contexts/ThemesContext";
 
 export const themes = {
     light: {
@@ -11,3 +14,18 @@ export const themes = {
     }
 }
 
+export const GlobalStyle = createGlobalStyle`
+
+body {
+        background-color: ${(props) => props.theme.background};
+        color: ${(props) => props.theme.color};
+        transition: background-color 0.3s ease, color 0.3s ease;
+    }
+;
+
+`
+
+export const ThemeGlobalStyle = () => {
+    const { theme } = useContext(ThemesContext);
+    return <GlobalStyle theme={theme} />;
+};
