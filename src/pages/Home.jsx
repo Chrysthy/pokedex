3f9bc3170a193b9b90f36b5f2ea15b2753c5079b
Chrystine Martins <chrystine.martins@outlook.com>
2025-07-14 20:21:20 -0300

    add Home file

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { fetchData } from "../api/api"
+
+export default function Home() {
+    
+    return (
+        <div>
+            <h1>Lista de Pokémons</h1>
+          
+        </div>
+    )
+}
