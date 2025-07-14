743e97e9aa5ba0877e1c710bce59507c38b97573
Chrystine Martins <chrystine.martins@outlook.com>
2025-07-14 19:57:55 -0300

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import { themes } from './styles/themes'
+import { ThemeGlobalStyle } from './styles/themes'
 import { ThemeProvider } from './contexts/ThemesContext'
 import { ThemeTogglerButton } from './components/ThemeTogglerButton/ThemeTogglerButton'
-import { createGlobalStyle } from 'styled-components'
 
 function App() {
 
   return (
-    <>
-      <ThemeProvider>
 
-        <ThemeTogglerButton />
+    <ThemeProvider>
 
-      </ThemeProvider>
+      <ThemeGlobalStyle />
+      <ThemeTogglerButton />
 
-    </>
+    </ThemeProvider>
   )
 }
-
-const GlobalStyle = createGlobalStyle`
-
-* {
-  margin:0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-`
 
 export default App;
